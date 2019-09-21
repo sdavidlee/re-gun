72c93f68420a1d62631128726c59b069ee1e10ad
violinoleum <55610241+violinoleum@users.noreply.github.com>
2019-09-21 16:43:38 -0500

    Get the Button to Change Scene
    Gets the button to change scene.

---
+++ Assets/Scripts/ButtonNewScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonNewScene : MonoBehaviour
+{
+    public string nextLevel;
+    public void onClick()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+}
